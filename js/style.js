6efa6dfa8f89a55b9008ae5e0f644d4332c1f858
Felipe Miguel <feliphe@tuta.io>
2023-02-27 20:55:04 -0300

    Deliver

--- js/style.js
+++ js/style.js
@@ -4,4 +4,6 @@ function OnIfood(){
     document.getElementById("next-if-2").style.transition = "1s";
     document.getElementById("next-if-2").style.backgroundColor = "#ea1d2bcb";
     document.getElementById("next-if-2").style.color = "#fff";
+    // ! END - ETAPA 1 
+    
 }
