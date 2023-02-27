1a3cce6941758537281d97f0706f6936e22d3c5d
Felipe Miguel <feliphe@tuta.io>
2023-02-27 20:37:39 -0300

    Deliver

--- js/style.js
+++ js/style.js
@@ -1,6 +1,7 @@
 function OnIfood(){
     document.getElementById("conteiner-row-ifood").style.display = "none";
-    document.getElementById("conteiner-row-ifood-premicao").style.display = "none";
+    document.getElementById("selects-vouchers-row").style.display = "none";
+    document.getElementById("next-if-2").style.backgroundColor = "";
 }
 function OffIfood(){
     alert("OffIfood")
